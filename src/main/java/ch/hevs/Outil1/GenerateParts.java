662e87c3e40c3aed35f9ba79902ed9df8c45a8d2
Elias <borrajo.elias@gmail.com>
2021-10-31 00:35:16 +0200

    Preparé la structure des classes & packages

--- src/main/java/ch/hevs/Outil1/GenerateParts.java
+++ src/main/java/ch/hevs/Outil1/GenerateParts.java
@@ -1,5 +1,23 @@
 package ch.hevs.Outil1;
 
+/**
+ * 1) Génerer les parts de secrets et en obtenir un fichier.
+ *      - Entrées   : #Bytes (modulo) / #Parts (10 employés) / Seuil (k  <= # Parts)
+ *      - Sortie    : #Fichier en fonction de #Parts. Fichier JSON contenant les parts de secrets
+ *      - Process   : Le degrée du polynome sera = seuil k-1, et nous génerera les parts a0, a1,..., an-1
+ *                    Puis on évalue f(x) = ax^2 + bx +c (dépend du degré) --> pour X
+ */
+
+/**
+ * OUTIL 1 :
+ * Ces calculs auront besoin de la classe dans MATHS pour faire ces opérations !
+ *
+ * Methode pour calculer : Li(x) = (x-xi)/(xm-xi)
+ *
+ * Methode pour calculer : f(x) = y = secret = SOMME( Li(x) * yi )
+ */
+
+
 public class GenerateParts
 {
 }
--- src/main/java/ch/hevs/Outil2/EncrypAndDecrypt.java
+++ src/main/java/ch/hevs/Outil2/EncrypAndDecrypt.java
@@ -1,12 +1,12 @@
 package ch.hevs.Outil2;
 
+
 /**
- * OUTIL 2 :
- * Ces calculs auront besoin de la classe dans MATHS pour faire ces opérations !
+ * 2) Encrypter / Decrypter un fichier
+ *      - Entrées   : # Parts(en fichier avec leur contenu en code) / Fichier à traiter / EncryptOrDecrypt
+ *      - Sortie    : Fichier d'entrée Decrypté OU Encrypté selon choix.
+ *      - Process   : Va calculer le secret en interne, il reste en mémoire.
  *
- * Methode pour calculer : Li(x) = (x-xi)/(xm-xi)
- *
- * Methode pour calculer : f(x) = y = secret = SOMME( Li(x) * yi )
  */
 public class EncrypAndDecrypt
 {
