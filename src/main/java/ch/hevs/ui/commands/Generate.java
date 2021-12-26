8b9012c2689aabb83d64e215951a673948865013
EliasKelliwich <borrajo.elias@gmail.com>
2021-12-26 15:20:01 +0100

    Ajout du README.md Changé le POM de JAVA veersion de 15 à 17

--- src/main/java/ch/hevs/ui/commands/Generate.java
+++ src/main/java/ch/hevs/ui/commands/Generate.java
@@ -10,19 +10,20 @@ import picocli.CommandLine;
         description = "Generate the Shamir secret parts",
         optionListHeading = "%nOptions are:%n")
 
-public class Generate implements Runnable{
+public class Generate implements Runnable
+{
     //*******************************************************************************
     //  A T T R I B U T S
     //*******************************************************************************
 
     // Generate parts
-    @CommandLine.Option(names = { "-g", "--GenPart" }, required = true, description = "Generate parts")
+    @CommandLine.Option(names = {"-g", "--GenPart"}, required = true, description = "Generate parts")
     boolean GenPart = false;
 
     // #Bytes (modulo)
     @CommandLine.Parameters(paramLabel = "<bytes>",
             description = "Modulo")
-    private byte bytes;
+    private int bytes; //@TODO, ce sont des int et non des bytes ;)
 
     // #Parts
     @CommandLine.Parameters(paramLabel = "<parts>", description = "Number of parts to be generated")
@@ -36,7 +37,8 @@ public class Generate implements Runnable{
     //  M E T H O D S
     //*******************************************************************************
     @Override
-    public void run() {
+    public void run()
+    {
         System.out.println("Generate parts run");
 
         GenerateParts gnP = new GenerateParts(bytes, parts, threshold);
